ea98e4121e12ee587eb7d5c19a8fbf8618ca2b55
Mohit kumar <mohitofficial1997@gmail.com>
2020-12-16 19:36:24 +0530

    deploy test 10

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app)
+    # from waitress import serve
+    # serve(app)
+    app.run()
